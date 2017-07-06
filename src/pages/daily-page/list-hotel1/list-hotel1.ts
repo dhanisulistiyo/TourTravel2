3da264e5bc0d5aea89bf8b4775926ed5ebe74bc9
DHANI SULISTIYO WIBOWO <dhani.s.wibowo@gmail.com>
2017-07-06 17:11:18 +0700

    update list-hotel

--- src/pages/daily-page/list-hotel1/list-hotel1.ts
+++ src/pages/daily-page/list-hotel1/list-hotel1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController} from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { HotelRoomtypePage1 } from '../hotel-roomtype1/hotel-roomtype1';
 import { AcomodationService } from '../../../providers/acomodation-service';
 import { FilterHotel1Page } from '../filter-hotel1/filter-hotel1';
@@ -21,7 +21,7 @@ export class ListHotelPage1 {
     public aco: AcomodationService,
     public ds: DailyService,
     public load: LoadingController
-  ) { 
+  ) {
     this.des = navParams.data['des']
     this.idAwal = navParams.data['id']
     this.idAkhir = navParams.data['i']
@@ -58,19 +58,20 @@ export class ListHotelPage1 {
     // set q to the value of the searchbar
     var q = searchbar.target.value;
     // if the value is an empty string don't filter the items
-    if (q.trim() == '') {
-      this.listHotel();
-      return;
-    }
-
-    this.hotels = this.hotels.filter((v) => {
-
-      if (v.Name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
-        return true;
+    if (q != undefined) {
+      if (q.trim() == '') {
+        this.listHotel();
+        return;
       }
-      return false;
-    })
 
+      this.hotels = this.hotels.filter((v) => {
+
+        if (v.Name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      })
+    } else this.listHotel();
   }
 
 
@@ -81,11 +82,11 @@ export class ListHotelPage1 {
     let i = this.idAkhir;
     let no = Object.keys(this.ds.daily[id].program_daily).length;
     console.log(no);
-    for(let j = 0 ; j < no ; j++){
+    for (let j = 0; j < no; j++) {
       this.ds.daily[id].program_daily[j].acomodation = null;
     }
     this.ds.setAcomodation(id, i, hot)
-    this.navCtrl.push(HotelRoomtypePage1,{id, i, ser});
+    this.navCtrl.push(HotelRoomtypePage1, { id, i, ser });
   }
 
 
@@ -97,7 +98,7 @@ export class ListHotelPage1 {
     let des = this.des;
     let id = this.idAwal;
     let i = this.idAkhir;
-    this.navCtrl.push(FilterHotel1Page ,{ des, id, i});
+    this.navCtrl.push(FilterHotel1Page, { des, id, i });
   }
 
 }
