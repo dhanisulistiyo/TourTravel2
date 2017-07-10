9207b4f21c7a27e313f0a7e8c0562bbce27f2c66
DHANI SULISTIYO WIBOWO <dhani.s.wibowo@gmail.com>
2017-07-10 12:43:55 +0700

    update screen input guest

--- src/pages/input-travellers/input-travellers.ts
+++ src/pages/input-travellers/input-travellers.ts
@@ -15,48 +15,85 @@ import {IteneraryService} from '../../providers/itenerary-service';
 })
 export class InputTravellersPage {
   guestTour = { AdultQty: null, ChildQty: null, InfantQty: null};
+  total;
 
   constructor(public navCtrl: NavController, public ite : IteneraryService, public alertCtrl: AlertController) {
     this.guestTour = { AdultQty: 0, ChildQty: 0, InfantQty: 0}
+    this.total = 0;
   }
 
   incrAdultQty(index:number){
     this.guestTour.AdultQty += 1;
+    console.log(this.guestTour.AdultQty);
+    this.total +=1;
   }
 
   incrChildQty(index:number){
     this.guestTour.ChildQty += 1;
+    console.log(this.guestTour.ChildQty);
+    this.total +=1;
   }
 
   incrInfantQty(index:number){
     this.guestTour.InfantQty += 1;
+    console.log(this.guestTour.InfantQty);
+    this.total +=1;
   }
 
   decrAdultQty(index:number){
-    if(this.guestTour.AdultQty < 0){
-         this.guestTour.AdultQty = 0;  
-    }
+    if(this.guestTour.AdultQty != 0){
     this.guestTour.AdultQty -= 1;
+    console.log(this.guestTour.AdultQty);
+    this.total -=1;
+    }
   }
 
   decrChildQty(index:number){
-    if(this.guestTour.ChildQty < 0){
-         this.guestTour.ChildQty = 0;  
-    }
+    if(this.guestTour.ChildQty != 0){
     this.guestTour.ChildQty -= 1;
+    console.log(this.guestTour.ChildQty);
+    this.total -=1;
+    }
   }
 
   decrInfantQty(index:number){
-    if(this.guestTour.InfantQty < 0){
-         this.guestTour.InfantQty = 0;  
-    }
+    if(this.guestTour.InfantQty != 0){
     this.guestTour.InfantQty -= 1;
-
+    console.log(this.guestTour.InfantQty);
+    this.total -=1;
+    }
   }
   
+  validateNumber(guestTour){
+      console.log(guestTour);
+      let ga = Number(guestTour.AdultQty)
+      let gc = Number(guestTour.ChildQty)
+      let gi = Number(guestTour.InfantQty)
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InputTravellersPage');
+      if (typeof ga != "number"  || String(ga) == "NaN" ){
+        this.showAlertAdult();
+        this.guestTour.AdultQty = 0;
+        this.total = this.guestTour.AdultQty + this.guestTour.ChildQty + this.guestTour.InfantQty;
+        return
+      }
+      else if (typeof gc != "number" || String(gc) == "NaN" ){
+        this.showAlertChild();
+        this.guestTour.ChildQty = 0;
+        this.total = this.guestTour.AdultQty + this.guestTour.ChildQty + this.guestTour.InfantQty;
+        return
+      }
+      else if(typeof gi != "number"  || String(gi) == "NaN" ){
+        this.showAlertInfant();
+        this.guestTour.InfantQty = 0;
+        this.total = this.guestTour.AdultQty + this.guestTour.ChildQty + this.guestTour.InfantQty;
+        return
+      }else{
+        this.guestTour.AdultQty = ga
+        this.guestTour.ChildQty = gc
+        this.guestTour.InfantQty = gi
+        this.total = ga+gc+gi;
+      }
+
   }
 
   doneTapped(guestTour) {
