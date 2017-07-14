eee2d788c6484b738cad4d6dcae8504e8247bc7c
ucunurul <ucu.nurul.ulum@basajans.com>
2017-07-14 18:19:58 +0700

    Update Screen Hotel, List, Attraction etc
    Update Screen Hotel, List, Attraction etc

--- www/build/4.main.js
+++ www/build/4.main.js
@@ -1,14 +1,14 @@
 webpackJsonp([4],{
 
-/***/ 330:
+/***/ 332:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MybookingCancelModule", function() { return MybookingCancelModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(3);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__mybooking_cancel__ = __webpack_require__(125);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MybookingCancelModule", function() { return MybookingCancelModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
