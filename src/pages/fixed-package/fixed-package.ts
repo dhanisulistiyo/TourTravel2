edc03127605a27d78e8993387f83a1fe60a70c29
ucunurul <ucu.nurul.ulum@basajans.com>
2017-07-25 09:26:08 +0700

    Update Fixed Package Screen v1
    Update Fixed Package Screen v1

--- src/pages/fixed-package/fixed-package.ts
+++ src/pages/fixed-package/fixed-package.ts
@@ -7,7 +7,13 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class FixedPackagePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  // sections;
+  items;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // this.sections = ['Culture 1', 'Culture 2', 'Culture 3']
+  this.items = [[0, 1, 2], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FixedPackagePage');
