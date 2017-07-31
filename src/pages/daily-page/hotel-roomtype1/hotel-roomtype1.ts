6f2f929362436cb507960be58c47160a374db276
ucunurul <ucu.nurul.ulum@basajans.com>
2017-07-31 17:04:00 +0700

    Update Screen Hotel Layout, Bug Fixes, etc
    Update Screen Hotel Layout, Bug Fixes, etc

--- src/pages/daily-page/hotel-roomtype1/hotel-roomtype1.ts
+++ src/pages/daily-page/hotel-roomtype1/hotel-roomtype1.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {HotelRoomservicePage1} from '../hotel-roomservice1/hotel-roomservice1';
-import {AcomodationService} from '../../../providers/acomodation-service';
-import {DailyService} from '../../../providers/daily-service';
+import { HotelRoomservicePage1 } from '../hotel-roomservice1/hotel-roomservice1';
+import { AcomodationService } from '../../../providers/acomodation-service';
+import { DailyService } from '../../../providers/daily-service';
 
 @Component({
   selector: 'page-hotel-roomtype1',
@@ -10,15 +10,16 @@ import {DailyService} from '../../../providers/daily-service';
   providers: [AcomodationService]
 })
 export class HotelRoomtypePage1 {
+  showToolbar: boolean = false;
 
-  listroomtypes:Array<any>;
+  listroomtypes: Array<any>;
   idAwal;
   idAkhir;
   idhot;
-  constructor(public navCtrl: NavController, 
-  public navParams: NavParams,
-  public ds : DailyService,
-  public aco : AcomodationService
+  constructor(public navCtrl: NavController, public ref: ChangeDetectorRef,
+    public navParams: NavParams,
+    public ds: DailyService,
+    public aco: AcomodationService
   ) {
     this.idhot = navParams.data['ser']
     this.idAwal = navParams.data['id']
@@ -26,28 +27,34 @@ export class HotelRoomtypePage1 {
 
   }
 
+  onScroll($event: any) {
+    let scrollTop = $event.scrollTop;
+    this.showToolbar = scrollTop >= 120;
+    this.ref.detectChanges();
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad HotelRoomtypePage');
   }
 
-    ionViewWillEnter() {
-    this.aco.getListItemAcomodation(this.idhot).subscribe(data=>{
-                this.listroomtypes=data;
-                console.log(this.listroomtypes);
-            },err => {
-                    console.log(err);
-                },
-                () => console.log('Room Type Search Complete')
-            );
+  ionViewWillEnter() {
+    this.aco.getListItemAcomodation(this.idhot).subscribe(data => {
+      this.listroomtypes = data;
+      console.log(this.listroomtypes);
+    }, err => {
+      console.log(err);
+    },
+      () => console.log('Room Type Search Complete')
+    );
   }
 
   setSelectedRoomAllo(itemroom) {
-     console.log(itemroom);
-     let id = this.idAwal;
-     let i = this.idAkhir;
-     let type = (itemroom['AccommodationItemServiceTypes']);
-     this.ds.setRoomType(id,i,itemroom);
-     this.navCtrl.push(HotelRoomservicePage1,{id,i, type});
+    console.log(itemroom);
+    let id = this.idAwal;
+    let i = this.idAkhir;
+    let type = (itemroom['AccommodationItemServiceTypes']);
+    this.ds.setRoomType(id, i, itemroom);
+    this.navCtrl.push(HotelRoomservicePage1, { id, i, type });
   }
-  
+
 }
