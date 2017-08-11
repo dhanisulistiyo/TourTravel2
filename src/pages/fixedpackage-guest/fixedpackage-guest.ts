ff181287808452a1f6aaf901e1e6e115fc03b582
ucunurul <ucu.nurul.ulum@basajans.com>
2017-08-11 15:37:14 +0700

    Delete unused code in fixed package guest.ts
    Delete unused code in fixed package guest.ts

--- src/pages/fixedpackage-guest/fixedpackage-guest.ts
+++ src/pages/fixedpackage-guest/fixedpackage-guest.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-=======
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
->>>>>>> 93bc1ddf29c8f60ae4878ae6127cea5dc3d03035
 
 /**
  * Generated class for the FixedpackageGuestPage page.
