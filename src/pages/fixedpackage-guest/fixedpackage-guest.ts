619fe6e6c7a0fa8362f713b5c9a1c8b16c36504f
ucunurul <ucu.nurul.ulum@basajans.com>
2017-08-16 15:03:56 +0700

    Update screen fixed package
    Update screen fixed package

--- src/pages/fixedpackage-guest/fixedpackage-guest.ts
+++ src/pages/fixedpackage-guest/fixedpackage-guest.ts
@@ -13,10 +13,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FixedpackageGuestPage {
   showToolbar: boolean = false;
+  guestTour = { AdultQty: null, ChildQty: null, InfantQty: null };
+  total;
+  maxGuest;
+  kuota;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public ref: ChangeDetectorRef) {
+    this.guestTour = { AdultQty: 0, ChildQty: 0, InfantQty: 0 };
+    this.total = 0;
+    this.kuota = navParams.data['kuota'];
   }
-  onScroll($event: any){
+  incrAdultQty(index: number) {
+      this.guestTour.AdultQty += 1;
+      console.log(this.guestTour.AdultQty);
+      this.total += 1;
+  }
+  incrChildQty(index: number) {
+      this.guestTour.ChildQty += 1;
+      console.log(this.guestTour.ChildQty);
+      this.total += 1;
+  }
+  incrInfantQty(index: number) {
+      this.guestTour.InfantQty += 1;
+      console.log(this.guestTour.InfantQty);
+      this.total += 1;
+  }
+  decrAdultQty(index: number) {
+    if (this.guestTour.AdultQty != 0) {
+      this.guestTour.AdultQty -= 1;
+      console.log(this.guestTour.AdultQty)
+      this.total -= 1;
+    }
+  }
+  decrChildQty(index: number) {
+    if (this.guestTour.ChildQty != 0) {
+      this.guestTour.ChildQty -= 1;
+      console.log(this.guestTour.ChildQty)
+      this.total -= 1;
+    }
+  }
+  decrInfantQty(index: number) {
+    if (this.guestTour.InfantQty != 0) {
+      this.guestTour.InfantQty -= 1;
+      console.log(this.guestTour.InfantQty)
+      this.total -= 1;
+    }
+  }
+
+  onScroll($event: any) {
     let scrollTop = $event.scrollTop;
     this.showToolbar = scrollTop >= 120;
     this.ref.detectChanges();
